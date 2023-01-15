627b02ff32b35550284b89ab1ab1fe1f9ed7afd7
yin.zhang <yin.zhang@mihoyo.com>
2023-01-15 21:45:58 +0800

    Release v1.0.2

--- tools.rb
+++ tools.rb
@@ -1,8 +1,8 @@
 class Tools < Formula
     desc "a cli tool to get information."
     homepage "https://github.com/stuchilde/tellme"
-    url "https://github.com/stuchilde/tellme/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "42e0735ca008dbd82ebef1b79bc0202232b3fcb895951c834a4fd7a879f4be1f"
+    url "https://github.com/stuchilde/tellme/archive/refs/tags/v1.0.2.tar.gz"
+    sha256 "605ffb285db9f5c83b086e2b075ad4a530a1087f628dbf3a2cec4a1d3f822fb0"
   
     depends_on "go" => :build
   
