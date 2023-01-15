5f94f901fdf249563e691dcf4ed941d72feab764
yin.zhang <yin.zhang@mihoyo.com>
2023-01-15 21:33:42 +0800

    Adjust directory

--- tools.rb
+++ tools.rb
@@ -2,13 +2,13 @@ class Tools < Formula
     desc "a cli tool to get information."
     homepage "https://github.com/stuchilde/tellme"
     url "https://github.com/stuchilde/tellme/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "42e0735ca008dbd82ebef1b79bc0202232b3fcb895951c834a4fd7a879f4be1f"
+    sha256 "4aafd7ba2879a5429c79cfb87218fa6763a882f5b94148b988a59a6ba2011605"
   
     depends_on "go" => :build
   
     def install
       system "go", "build", "-o", bin/"tellme"
-      system "cp", "bin/IP2LOCATION-LITE-DB3.BIN", bin/"bin/IP2LOCATION-LITE-DB3.BIN"
+      system "cp", "bin/IP2LOCATION-LITE-DB3.BIN", bin/"IP2LOCATION-LITE-DB3.BIN"
     end
   
     test do
