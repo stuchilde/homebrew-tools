c72641a1ed7b4269b65a11c8fad9ce410f220e7f
yin.zhang <yin.zhang@mihoyo.com>
2023-01-15 20:33:31 +0800

    Adjust file sha256

--- tools.rb
+++ tools.rb
@@ -2,7 +2,7 @@ class Tools < Formula
     desc "a cli tool to get information."
     homepage "https://github.com/stuchilde/tellme"
     url "https://github.com/stuchilde/tellme/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "1dd457995bcac4bfee4c338c26a5a22b0307139b4857a6a157ddd7a7aa10d5e5"
+    sha256 "4aafd7ba2879a5429c79cfb87218fa6763a882f5b94148b988a59a6ba2011605"
   
     depends_on "go" => :build
   
